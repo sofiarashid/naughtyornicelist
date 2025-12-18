3c3bf7b10ef33d74125a9da6df9cab0cd281c066
sofiarashid <527srashid@frhsd.com>
2025-12-18 09:31:49 -0500

    basic form done

---
+++ script.js
@@ -0,0 +1,25 @@
+function getPoints(value) {
+    switch (value) {
+        case 'nice':
+            return 1
+        case 'naughty':
+            return -1
+        default:
+            return -1
+    }
+}
+
+function checkNaughtyOrNice() {
+    const q1 =  document.getElementById('q1').value
+    const q2 =  document.getElementById('q2').value
+    const q3 =  document.getElementById('q3').value
+    const q4 =  document.getElementById('q4').value
+
+    const score = getPoints(q1) + getPoints(q2) + getPoints(q3) + getPoints(q4) + getPoints(q5)
+
+    if (score > 2.5) {
+        alert('You are on the NICE list!')
+    } else {
+        alert('You are on the NAUGHTY list!')
+    }
+}
